f1531eedc304e7169ef0a780afee577c9a7966bf
mr-wolfdog <62209904+mr-wolfdog@users.noreply.github.com>
2020-09-08 12:55:57 -0500

    Add files via upload
    This is the initial commit that includes the installer script, the main script for the tool (hat), and the sampleScanner script (which performs the VirusTotal scanning).

---
+++ installer.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#This is a script designed to install the HAT tool and some of its dependencies
+
+echo "Welcome to the Honeypot Administration Tool (HAT) installer!"
+
+#Make sure that the user has their own bin directory
+if ! [ -d "~/bin/" ]
+then
+	mkdir ~/bin/
+fi
+
+#Move the script files into the user's bin directory
+find . -type f -not -name "installer.sh" -exec cp {} ~/bin/
+
+#Begin by cloning the GitHub page for the VirusTotal API that will be used
+git clone http://github.com/doomedraven/VirusTotalApi
+
+cd VirusTotalApi/
+./setup.py
+pip3 install -r requirements.txt
+
+#Copy vt.py, the necessary script, into the user's bin
+cp vt/vt.py ~/bin/vt
+
+#Move back to the installer's directory
+cd ..
+
+echo "Try out the scripts now and see if they work! Enjoy malware catching!"
