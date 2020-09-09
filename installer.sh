333c66c301d023df45d0afce8f25def8ea320858
mr-wolfdog <62209904+mr-wolfdog@users.noreply.github.com>
2020-09-09 11:10:30 -0500

    Update installer.sh
    This is a fix of some security/usability issues with the previous version of the script. The changes were made with the advice of https://www.shellcheck.net/ .

--- installer.sh
+++ installer.sh
@@ -5,18 +5,18 @@
 echo "Welcome to the Honeypot Administration Tool (HAT) installer!"
 
 #Make sure that the user has their own bin directory
-if ! [ -d "~/bin/" ]
+if ! [ -d "$HOME/bin/" ]
 then
 	mkdir ~/bin/
 fi
 
 #Move the script files into the user's bin directory
-find . -type f -not -name "installer.sh" -exec cp {} ~/bin/
+find . -type f -not -name "installer.sh" -exec cp {} ~/bin/ +
 
 #Begin by cloning the GitHub page for the VirusTotal API that will be used
 git clone http://github.com/doomedraven/VirusTotalApi
 
-cd VirusTotalApi/
+cd VirusTotalApi/ || exit
 ./setup.py
 pip3 install -r requirements.txt
 
